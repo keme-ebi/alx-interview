91354cf962a40584e4185d8b7090a6c4dd810a51
Remember <kelvin6bolou@gmail.com>
2024-03-18 10:43:49 +0100

    added comments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,11 +15,14 @@ def pascal_triangle(n):
     if (n <= 0):
         return []
 
-    pascal_list = [[1]]
+    pascal_list = [[1]] # first row on the triangle
+
     for i in range(1, n):
-        row = [1]  # First element of each row is always 1
+        row = [1]  # start the row with 1
         for j in range(1, i):
             row.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
-        row.append(1)  # Last element of each row is always 1
-        pascal_list.append(row)
+        row.append(1)  # add 1 to the end of the row
+
+        pascal_list.append(row) # append row to the list of lists
+
     return pascal_list
