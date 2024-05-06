edf5f0c10e76e3b2b1c5c1580c5cbb838f922b91
keme-ebi <kelvin6bolou@gmail.com>
2024-05-07 00:36:44 +0100

    island perimeter challenge

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""island perimeter"""
+
+
+def island_perimeter(grid):
+    """gets the perimeter of an island described in grid
+    Args:
+        grid: a 2d list of integers
+    Return:
+        the perimeter of the island
+    """
+    """
+    first two tuple signify rows, last two signify columns
+    will be used as offsets of the grid
+    in computer, y axis decreases upwards and increases downwards
+    x axis increases towards the rigth, and decreases left
+    (up(-y), down(y), left(-x), right(x))
+    """
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    rows = len(grid) # length of a row in the grid
+    columns = len(grid[0]) # length of a column in the grid
+
+    perimeter = 0 # total number of perimeter
+
+    for ro in range(rows):
+        for col in range(columns):
+            if grid[ro][col] == 1:
+                zeros = 0
+
+                # d_ro = row direction, d_co = column direction
+                for d_ro, d_co in direction:
+                    off_ro = ro + d_ro # offset row
+                    off_co = col + d_co # offset column
+
+                    # check if offset is within bounds
+                    if 0 <= off_ro < rows and 0 <= off_co < columns:
+                        # increment zero if offset of 1 island cell is zero
+                        if grid[off_ro][off_co] == 0:
+                            zeros += 1
+
+                perimeter += zeros
+
+    # return total perimeter
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,2 @@
+# Island Perimeter
+The goal is to calculate the perimeter of a single island in a grid, where the grid is represented by a 2D array of integers.
