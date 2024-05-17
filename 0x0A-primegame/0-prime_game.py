cb20a5833e984bec3f7ea37514926df8b292f085
keme-ebi <kelvin6bolou@gmail.com>
2024-05-17 02:38:14 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""prime game"""
+
+
+def soe(n):
+    """SieveOfEratosthenes
+    gets all the prime numbers to n
+    Arg:
+        n(int): number given
+    Return:
+        a list of prime numbers
+    """
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+
+    nums = [p for p in range(2, n + 1) if prime[p]]
+    return nums
+
+
+def isWinner(x, nums):
+    """gets the player that wins the most rounds
+    Args:
+        x(int): number of rounds
+        nums(int): an array of n
+    Return:
+        name of player that won most rounds
+    """
+    Maria = 0  # score for maria
+    Ben = 0  # score for ben
+    winner = None
+
+    # if the array is not empty
+    if nums:
+        for i in range(x):
+            maria = soe(nums[i])[0::2]  # gets primes for maria
+            ben = soe(nums[i])[1::2]  # gets primes for ben
+            if len(maria) > len(ben):
+                Maria += 1
+            if len(maria) == len(ben):
+                Ben += 1
+        winner = 'Maria' if Maria > Ben else 'Ben'
+
+    return winner
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,2 @@
+# Prime Game
+The challenge involves determining the winner of a game based on the strategic removal of prime numbers and their multiples from a consecutive integers
